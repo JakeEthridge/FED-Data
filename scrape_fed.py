6072246d4e9130817ff6d7ecbc940dfe1577e163
JakeEthridge <je327020@ohio.edu>
2025-01-18 00:36:08 -0500

    Output press release from most recent FOMC Statement

--- scrape_fed.py
+++ scrape_fed.py
@@ -1,12 +1,36 @@
 from urllib.request import Request, urlopen
+import re
 
+# Request Object
 req = Request(
-    url='https://www.federalreserve.gov/', 
-    headers={'User-Agent': 'Mozilla/5.0'}
+    url='https://www.federalreserve.gov/newsevents/pressreleases/monetary20241218a.htm', 
+    headers={'User-Agent': 'Mozilla/5.0'},
+    method = 'GET'
 )
 
+# Open URL
 webpage = urlopen(req)
+# Read HTML
 data = webpage.read()
-print(data)
+# Convert to String
+html_content = data.decode('utf-8')
+print('\033[1;92m' + html_content)
 
-print(type(data))
+# Title Of Article
+title_index = html_content.find('<title>')
+start_index = title_index + len('<title>')
+end_index = html_content.find('</title>')
+print('# ' + '=' * 78 + ' #')
+print(html_content[start_index:end_index])
+print('# ' + '=' * 78 + ' #')
+
+list = re.findall('<p>.*</p>', html_content, re.IGNORECASE)
+fed_speak = ""
+
+for i in list: 
+    i = i.replace('<p>', '')
+    i = i.replace('</p>', '')
+    fed_speak += i
+
+print(fed_speak)
+
